6b135e4e155ba944c80c1e91d8f88ce59aaa0955
swdowia1 <swdowia1@gazeta.pl>
2024-10-03 19:32:13 +0200

    statystyka

---
+++ AngielskiNauka/ModelApi/StatView.cs
@@ -0,0 +1,10 @@
+﻿namespace AngielskiNauka.ModelApi
+{
+    public class StatView
+    {
+        public string Data { get; set; }
+        public int Ilosc { get; set; }
+        public bool Powyzej { get; set; }
+        public string Poziom { get; set; }
+    }
+}
--- AngielskiNauka/Pages/Stat.cshtml.cs
+++ AngielskiNauka/Pages/Stat.cshtml.cs
@@ -1,11 +1,33 @@
+using AngielskiNauka.ModelApi;
+using AngielskiNauka.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 
 namespace AngielskiNauka.Pages
 {
     public class StatModel : PageModel
     {
+        public List<StatView> stat;
+        AaaswswContext _db;
+
+        public StatModel(AaaswswContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            CultureInfo polish = new CultureInfo("pl-PL");
+            stat = _db.Stats.Where(w => w.Data > DateTime.Now.AddDays(-30)).OrderByDescending(j => j.Data).Select(h =>
+
+               new StatView()
+               {
+                   Data = h.Data.ToString("MMMM d (dddd)", polish) + " " + h.Data.ToString("HH:mm"),
+                   Ilosc = h.Ilosc,
+                   Powyzej = h.Ilosc > 80,
+                   Poziom = h.Poziom.Nazwa
+               }
+           ).ToList();
         }
     }
 }
