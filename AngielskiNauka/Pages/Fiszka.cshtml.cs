34516ae6b63ee8a632d1a92a30c4d172c83d4e64
swdowia1 <swdowia1@gazeta.pl>
2024-11-24 17:33:30 +0100

    fiszki i mnozenie

---
+++ AngielskiNauka/Pages/Fiszka.cshtml.cs
@@ -0,0 +1,33 @@
+using AngielskiNauka.ModelApi;
+using AngielskiNauka.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AngielskiNauka.Pages
+{
+    public class FiszkaModel : PageModel
+    {
+
+
+        public List<Slowo> List;
+        AaaswswContext _db;
+
+        public FiszkaModel(AaaswswContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int id = 1)
+        {
+            List = _db.Danes.ToList().OrderBy(d => d.Data).Where(p => p.PoziomId == id).Select(
+                (j, index) => new Slowo()
+                {
+                    Id = index + 1,
+                    Pol = j.Pol,
+                    Ang = j.Ang
+                }).ToList();
+
+
+        }
+    }
+}
