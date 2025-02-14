91ac83795b1af2dc49d7f20416af337aff6b78d6
Sławomir Wdowiak <slawomir.wdowiak@onninen.com>
2025-02-14 13:31:33 +0100

    waluty

---
+++ AngielskiNauka/ModelApi/ExchangeRateResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace AngielskiNauka.ModelApi
+{
+    public class ExchangeRateResponse
+    {
+        public List<Rate> rates { get; set; }
+    }
+
+    public class Rate
+    {
+        public string effectiveDate { get; set; }
+        public double mid { get; set; }
+    }
+}
