cd7da33d1530b63eed1512e33efeb29be24aac88
Sławomir Wdowiak <swdowiak1@outlook.com>
2025-04-09 09:10:55 +0200

    edycja slowek

---
+++ AngielskiNauka/ModelApi/DaneUpdate.cs
@@ -0,0 +1,9 @@
+﻿namespace AngielskiNauka.ModelApi
+{
+    public class DaneUpdate
+    {
+        public int Id { get; set; }
+        public string Pol { get; set; }
+        public string Angedit { get; set; }
+    }
+}
---
+++ AngielskiNauka/bazaController.cs
@@ -0,0 +1,21 @@
+﻿using AngielskiNauka.ModelApi;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngielskiNauka
+{
+    [Route("api/[controller]")]
+    //[Route("api/ang")]
+    [ApiController]
+    public class bazaController : ControllerBase
+    {
+        // Czwarta metoda GET zwracająca listę Job
+        [HttpPost("update")]
+        public async Task<ActionResult<int>> update([FromBody] DaneUpdate dane)
+        {
+
+
+
+            return new JsonResult(1);
+        }
+    }
+}
