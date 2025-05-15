a99edb7e0a3e7bc9662406c06db276a608e8bd3c
Sławomir Wdowiak <swdowiak1@outlook.com>
2025-05-15 20:35:03 +0200

    migracja

---
+++ AngielskiNauka/Migrations/20250515182012_dataAkt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AngielskiNauka.Migrations
+{
+    /// <inheritdoc />
+    public partial class dataAkt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataAkt",
+                table: "Danes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataAkt",
+                table: "Danes");
+        }
+    }
+}
