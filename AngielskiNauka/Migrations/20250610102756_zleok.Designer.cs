e4aa032be9ac9317736f554c7dad3bd46a80a182
swdowia1 <swdowia1@gazeta.pl>
2025-06-10 12:52:22 +0200

    statystkya dla poziomu

--- AngielskiNauka/Migrations/20250610102756_zleok.Designer.cs
+++ AngielskiNauka/Migrations/20250610102756_zleok.Designer.cs
@@ -4,6 +4,7 @@ using AngielskiNauka.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AngielskiNauka.Migrations
 {
     [DbContext(typeof(AaaswswContext))]
-    partial class AaaswswContextModelSnapshot : ModelSnapshot
+    [Migration("20250610102756_zleok")]
+    partial class zleok
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AngielskiNauka/Migrations/20250610102756_zleok.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AngielskiNauka.Migrations
+{
+    /// <inheritdoc />
+    public partial class zleok : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Ok",
+                table: "Stats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Zle",
+                table: "Stats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ok",
+                table: "Stats");
+
+            migrationBuilder.DropColumn(
+                name: "Zle",
+                table: "Stats");
+        }
+    }
+}
